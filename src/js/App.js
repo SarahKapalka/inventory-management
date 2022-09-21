25e21660f0269336f931bdf5e4035808aa5f0af1
C Jalloh <esjallow03@gmail.com>
2022-09-21 01:17:57 +0000

    something something

--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,9 @@ import Main from './Main';
 export default function App(){
     return(
         <div className='container'>
-        <h1>Store Inventory Management</h1>
+            <h1>Store Inventory Management</h1>
+            <h2>LINK UP ELECTRONICS</h2>
+            <h3>Great JOB GUYS :smiley::smiley_cat::fire:</h3>
         <Main/>
         </div>
     )
